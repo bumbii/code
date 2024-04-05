e0341460ee3f7abf38ee663f30310f4cb065137a
hautran90 <hau@bumbii.tech>
2024-04-05 17:22:49 +0700

    add bool

---
+++ python/tutorials/basics/bool.py
@@ -0,0 +1,34 @@
+print(20 > 10)
+
+print(20 < 10)
+
+print('a' < 'b')
+
+print('a' > 'b')
+
+# Hàm bool
+
+print(bool('Hi'))
+
+print(bool(''))
+
+print(bool(100))
+
+print(bool(0))
+
+# Toán tử
+
+print(2 < 10 and 2 < 40)
+
+print(10 > 2 > 40)
+
+print(2 < 10 or 2 < 40)
+
+print(2 < 10 or 2 > 40)
+
+print(2 > 10 or 2 > 40)
+
+num = 20
+print(not num < 20)
+
+print(not num > 10)
